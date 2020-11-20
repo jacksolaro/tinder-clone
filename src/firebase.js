0a8aad6f8ed35c1fec474f87239f1551934150df
jacksolaro <jacksolaro@gmail.com>
2020-11-19 16:48:58 -0800

    added firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMs-ojdx83UBSk_uF8DFGZnSC1aaj8pYc",
+  authDomain: "tinder-clone-f5df0.firebaseapp.com",
+  databaseURL: "https://tinder-clone-f5df0.firebaseio.com",
+  projectId: "tinder-clone-f5df0",
+  storageBucket: "tinder-clone-f5df0.appspot.com",
+  messagingSenderId: "135402692554",
+  appId: "1:135402692554:web:a807b0f3459f981c767933",
+  measurementId: "G-1TC87QQ1YH",
+};
